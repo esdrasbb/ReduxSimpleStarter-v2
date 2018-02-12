64f8809c1fd329d2ce5351b1590e99158ac5204b
Esdras Barreto <esdras.barreto@gmail.com>
2018-02-12 09:52:30 -0300

    Finished section 4

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
-export default function () {
+export default function() {
     return [
-        {title: "Javascript: The Good Parts"},
-        {title: "Harry Potter"},
-        {title: "The Dark Tower"},
-        {title: "Eloquent Ruby"}
-    ]
+        { title: "Javascript: The Good Parts", pages: 100 },
+        { title: "Harry Potter", pages: 35 },
+        { title: "The Dark Tower", pages: 85 },
+        { title: "Eloquent Ruby", pages: 10 }
+    ];
 }
